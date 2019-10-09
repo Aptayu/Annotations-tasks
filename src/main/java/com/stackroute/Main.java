0435c8f397be0eb3abaf3758ffe4436477816f21
Arpit <arpitarga@gmail.com>
2019-10-09 11:46:06 +0530

    Third commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -11,6 +11,8 @@ public class Main {
         Movie movie3 = (Movie) factory.getBean("movie1");
         Movie movie2 = (Movie) factory.getBean("movie1");
         System.out.println(movie3==movie2);
+//        difference here is prototype creates a new object but upper two lines only created one object and second object
+//        is only reference to the first object.
 
         Movie movieprototype = (Movie) factory.getBean("moviePrototype");
         System.out.println(movieprototype==movie3);
